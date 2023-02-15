ee2b01b9b75bbeeb94d0fefc1613cec59685e9cd
203423 <203423@ids.upchiapas.edu.mx>
2023-02-15 14:54:00 -0600

    commit 3

--- algoritmo_genetico.py
+++ algoritmo_genetico.py
@@ -1,8 +1,6 @@
 import random
 import itertools
-import math
 import grafica
-import graf_aptos
 from tkinter import *
 import tkinter as tk
 from numpy import mean
@@ -21,16 +19,10 @@ class Genes():
         self.probMuta = probMuta
         self.id = id
         self.muta = muta
-
-
 class Pareja:
     def __init__(self,idParejas,probCruza):
         self.idParejas = idParejas
         self.probCruza = probCruza
-
-
-
-#Metodo de seleccion
 def seleccion_par(pob):
     masApto = pob[0]
     parejas = []
@@ -44,71 +36,47 @@ def seleccion_par(pob):
         listID.append(pob[x].cromosoma)
     parejas = list(itertools.combinations(listID, 2))
     return parejas
-    
- 
 def decimal_a_binario(decimal):
     if decimal <= 0:
         return "0"
-    # Aquí almacenamos el resultado
     binario = ""
-    # Mientras se pueda dividir...
     while decimal > 0:
-        # Saber si es 1 o 0
         residuo = int(decimal % 2)
-        # E ir dividiendo el decimal
         decimal = int(decimal / 2)
-        # Ir agregando el número (1 o 0) a la izquierda del resultado
         binario = str(residuo) + binario
     return binario
-
 def binario_a_decimal(binario):
     return int(binario, 2)
-
 def calcularX(xmin,i,intervalo):
     return xmin + i * intervalo
-
-# Función cuadrática.
 def f1(x):
     return x **2 -2*x**3
-
 def porcentaje():
     valor = random.randint(0,100)
     valor = valor/100
     return valor
-
-
-
-#ESTOS METODOS SE ESTARAN REPITIENDO HASTA QUE EL ALGORITMO RECIBA SU VALOR PARA PARAR
 def nuevosIndividuos(POBLACION, numBits, xmin, xinterval,xmax,id_ind,PMAX):
-    #AQUI EMPIEZA LA CRUZA
-    #Seleccion de parejas que pueden cruzar
     PC = 0.60
-    parejas = seleccion_par(POBLACION) #todas las posibles parejas para cruza
+    parejas = seleccion_par(POBLACION) 
     print(str(parejas))
-
     paresCruzas = []
     for x in parejas:
         probCruza = porcentaje()
         if probCruza <= PC:
             temp = list(tuple(x))
-            paresCruzas.extend(temp) #Aqui se almacenan las parejas que si van a poder cruzarse
-
+            paresCruzas.extend(temp)
     print("ARR: ParesCruza:")
     print(paresCruzas)
-    #Cruza
     cruzBin = []
     for x in paresCruzas:
         for i in range(0,len(POBLACION)):
             if x == POBLACION[i].cromosoma:
-                cruzBin.append(POBLACION[i].cromosomaBinario) #CONVIERTE A BINARIO EL VALOR DE LOS INDIVIDUO A CRUZAR
+                cruzBin.append(POBLACION[i].cromosomaBinario) 
                 break
     print("CRUZBIN")
     print(cruzBin)
-
-    #Se define el punto de cruza de todos los individuos que se cruzaran
     puntoCruza =  round(5/2)
     newPob = []
-    #Se generan los nuevos individuos, que pasaran a la mutacion
     for x in range(0,len(cruzBin),2):
         val1 = cruzBin[x]
         val2 = cruzBin [x+1]
@@ -118,17 +86,11 @@ def nuevosIndividuos(POBLACION, numBits, xmin, xinterval,xmax,id_ind,PMAX):
         newVal2 = val2[0:puntoCruza] + val1[puntoCruza:]
         newPob.append(newVal1)
         newPob.append(newVal2)
-
     print("Poblacion antes de mutar:")
     print(newPob)
-
-    #MUTACION
     PMI = 0.60
     PMG = 0.50
-
-    ##MUTA EL INDIVIDUO?
     arrGenes = []
-
     for x in range(0,len(newPob)):
         PMI_ind = porcentaje()
         print("PROBABILIDAD QUE MUTE EL INDIVIDUO:"+str(PMI_ind))
@@ -142,12 +104,9 @@ def nuevosIndividuos(POBLACION, numBits, xmin, xinterval,xmax,id_ind,PMAX):
                 if PMG_ind <= PMG:
                     muta = True 
                 arrGenes.append(Genes(x,genes[i], PMG_ind,muta))
-            #MUTA POR GEN
-
     individuoNuevo = 0
     genmutado = ''
     genesmutados = []
-    
     for gen in arrGenes:
         genmutado = genmutado+gen.gen
         if len(genmutado) == numBits:
@@ -181,7 +140,6 @@ def nuevosIndividuos(POBLACION, numBits, xmin, xinterval,xmax,id_ind,PMAX):
     print(newGenes)
     print('ARREGLO DE LA POBLACION')
     print(newPob)        
-
     y = 0
     print("NEWPOB")
     print(newPob)
@@ -194,7 +152,6 @@ def nuevosIndividuos(POBLACION, numBits, xmin, xinterval,xmax,id_ind,PMAX):
     y = 0
     print("poblacion despues de mutar")
     print(newPob)
-
     PoblacionNueva = []
     for x in newPob:
         cromosoma = binario_a_decimal(x)
@@ -205,20 +162,15 @@ def nuevosIndividuos(POBLACION, numBits, xmin, xinterval,xmax,id_ind,PMAX):
             funcion = "OUT"
         PoblacionNueva.append(Individuo(cromosoma,xPob,funcion,id_ind,x)) 
         id_ind = id_ind +1
-
     print("POSIBLES POBLADORES NUEVOS")
     for poblador in PoblacionNueva:
         print("Individuo "+str(poblador.cromosoma) + " x del individuo "+str(poblador.x)+" Ind en binario "+str(poblador.cromosomaBinario)+ " y su aptitud es de:"+str(poblador.aptitud))
         if poblador.x != "OUT":
             POBLACION.append(poblador)
-            
-
     print("POBLACION DESPUES DE AGREGAR LOS NUEVOS INDIVIDUOS")
-
     POBLACION = sorted(POBLACION,key=lambda poblador: poblador.aptitud, reverse=True)
     for poblador in POBLACION:
         print("ID:"+str(poblador.id)+"| Individuo "+str(poblador.cromosoma) + " x del individuo "+str(poblador.x)+" Ind en binario "+str(poblador.cromosomaBinario)+ " y su aptitud es de:"+str(poblador.aptitud))
-
     #PODA
     numero_a_podar = len(POBLACION) - PMAX
     for i in range(numero_a_podar):
@@ -227,17 +179,7 @@ def nuevosIndividuos(POBLACION, numBits, xmin, xinterval,xmax,id_ind,PMAX):
     print("Nueva poblacion despues de la poda")
     for poblador in POBLACION:
         print("Individuo "+str(poblador.cromosoma) + " x del individuo "+str(poblador.x)+" Ind en binario "+str(poblador.cromosomaBinario)+ " y su aptitud es de:"+str(poblador.aptitud))
-    
     return POBLACION
-
-
-
-
-
-
-
-#CONFIGURAACIONES INICIALES DEL ALGORITMO
-
 def main(pobmax,generaciones,min,max):
     id_ind = 0
     PMAX = int(pobmax)
@@ -261,13 +203,8 @@ def main(pobmax,generaciones,min,max):
     print("La cantidad de bits es = "+ str(numBits))
     print("Poblacion Inicial: "+str(PINICIAL))
     print('La poblacion maxima es:'+str(PMAX))
-
-
-#IMPRIME EL CROMOSOMA DE LOS INDIVIDUOS
     for x in range(0,len(POBLACION)):
         print("EL cromosoma del individuo "+str(POBLACION[x].id) +" es: "+str(POBLACION[x].cromosoma))
-
-#SE CALCULA X DE LOS INDIVIDUOS, SU APTITUD Y SU BINARIO
     for i in range(0,len(POBLACION)):
         x = calcularX(xmin, POBLACION[i].cromosoma, xinterval)
         POBLACION[i].x = x
@@ -276,8 +213,6 @@ def main(pobmax,generaciones,min,max):
         while len(POBLACION[i].cromosomaBinario) < numBits:
             POBLACION[i].cromosomaBinario = "0"+ POBLACION[i].cromosomaBinario
         print("Individuo:"+str(POBLACION[i].id)+"Con cromosoma:" +str(POBLACION[i].cromosoma)+"- x: "+str(POBLACION[i].x)+" su aptitud es "+str(POBLACION[i].aptitud)+" y su binario es "+str(POBLACION[i].cromosomaBinario))
-
-#AQUI EMPIEZA LA CRUZA
     masApto = []
     menosApto = []
     promedio = []
@@ -294,17 +229,14 @@ def main(pobmax,generaciones,min,max):
     for apto in masApto:
         print(apto.x)
     
-    graf_aptos.graficar_aptos(masApto,menosApto,promedio)
+    grafica.graficar_aptos(masApto,menosApto,promedio)
     
 def send_data():
-  global total
   min_info = min.get()
   max_info = max.get()
   pobMax_info = pobMax.get()
   generaciones_info = generaciones.get()
 
- 
-  total = 3000
   main(pobMax_info,generaciones_info,min_info,max_info)
 
 if __name__ == "__main__": 
@@ -340,9 +272,3 @@ if __name__ == "__main__":
     submit_btn = Button(frame_izquierda, text="Continuar", command = send_data)
     submit_btn.place(x = 22, y = 480)
     ventana.mainloop()
-
-
-
-
-
-
--- grafica.py
+++ grafica.py
@@ -1,32 +1,60 @@
 from matplotlib import pyplot 
-import math
 # Función cuadrática.
 def f1(x):
-    return (x**2)*(math.e**x) *(math.cos(10*x))
+    return x **2 -2*x**3
 
 def graficar(POBLACION,generacion):
-    pyplot.title('Grafica de la funcion: f(x) = (x**2)*(math.e**x) *(math.cos(10*x)\n Generacion:'+str(generacion+1),color='red',size=10, family='arial')
+    pyplot.title('Grafica de la funcion: f(x) = (x**2)-(2x**3)\n Generacion:'+str(generacion+1),color='red',size=10, family='arial')
     print(POBLACION[0].x)
-    # Valores del eje X que toma el gráfico.
+    
     x = range(-70, 70)
-    # Graficar ambas funciones.
     pyplot.plot(x, [f1(i) for i in x])
-    # Establecer el color de los ejes.
     pyplot.axhline(0, color="black")
     pyplot.axvline(0, color="red")
-    # Limitar los valores de los ejes.
     pyplot.xlim(-14, 20)
     pyplot.ylim(-10, 30)
     pyplot.xlabel('X_axis\nIndividuo mas apto de esta generacion'+str(POBLACION[0].x)+' tiene una aptitud de: '+str(POBLACION[0].aptitud), size = 6)
     pyplot.ylabel('Y_axis', size = 14)
-
-    dots = []
     for individuo in POBLACION:
         pyplot.annotate("Individuo: "+str(individuo.cromosoma), (individuo.x, individuo.aptitud))
         dot = pyplot.scatter(individuo.x, individuo.aptitud)
 
-    # Guardar gráfico como imágen PNG.
     pyplot.savefig("output.png")
+    pyplot.show()
 
-    # Mostrarlo.
+def graficar_aptos(aptos,menosApto,promedio):
+    x1=[]
+    y1=[]
+    x2 = []
+    y2 = []
+    x3 = []
+    y3 = []
+
+    i = 1
+    for apto in aptos:
+        x1.append(i)
+        i = i+1
+        y1.append(apto.aptitud)
+    i = 1
+    for apto in menosApto:
+        x2.append(i)
+        i = i+1
+        y2.append(apto.aptitud)
+    i = 1
+    for prom in promedio:
+        x3.append(i)
+        i = i+1
+        y3.append(prom)
+    pyplot.title('Evolucion del individuo mas apto a traves de las generaciones',color='red',size=10, family='arial')
+
+
+    pyplot.plot(x1,y1,linestyle='solid',color='blue',label="mas aptos")
+    pyplot.plot(x1,y1,'o',color='black')
+    pyplot.plot(x2,y2,linestyle='solid',color='red',label="menos apto")
+    pyplot.plot(x2,y2,'o',color='black')
+    pyplot.plot(x3,y3,linestyle='solid',color='green',label="promedio")
+    pyplot.plot(x3,y3,'o',color='black')
+    pyplot.legend(loc ="upper left")
+    pyplot.xlabel('Numero de la generacion')
+    pyplot.ylabel('Aptitud')
     pyplot.show()
